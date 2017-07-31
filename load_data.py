bb1fb501e56e179fb1774f61f921f5f8aaccb51a
mostafa mahmoud <mostafa_mahmoud@protonmail.com>
2017-07-31 22:18:47 +0200

    Added visualization functions to load_data.py for dataset images and bad examples defines in a text file. Added a method to DataLoader that loads the data explicitly from csv, performs filtering and updates the npy files. Added a function that deletes npy generated files. Added a function that loads and parses the bad data indices text file into numpy array. Corrected function documentation.

--- load_data.py
+++ load_data.py
@@ -4,14 +4,16 @@
 import os
 import numpy as np
 import pandas as pd
+import cv2 as cv
 
 facial_expressions = ("Angry", "Disgust", "Fear", "Happy", "Sad", "Surprise", "Neutral")
 
 
-def load_data_from_csv(path="data/fer2013.csv"):
+def load_data_from_csv(path="data/fer2013.csv", filter_dataset=False):
     """
     Load data from kaggle csv file and parse to numpy arrays ready for training
     :param path: a relative path to the csv file from the project directory
+    :param filter_dataset: a flag whether to filter the dataset or not
     :return: train_x: numpy array
     :return: train_y: numpy array
     :return: test_x: numpy array
@@ -20,6 +22,11 @@ def load_data_from_csv(path="data/fer2013.csv"):
     path = os.path.join(os.path.dirname(__file__), path)
     assert os.path.isfile(path)
     df = pd.read_csv(path)
+
+    if filter_dataset:
+        bad_data_indices = get_bad_samples()
+        if bad_data_indices is not None:
+            pass
 
     # Data Transformation
     train_x = df.loc[df['Usage'].isin(['Training', 'PublicTest']), ['pixels']]
@@ -41,7 +48,7 @@ def save_data_to_npy(train_x, train_y, test_x, test_y, path="data"):
     :param train_y: numpy array
     :param test_x: numpy array
     :param test_y: numpy array
-    :param path: relative path to the current working directory
+    :param path: relative path to the project directory
     """
     path = os.path.join(os.path.dirname(__file__), path)
     np.save(os.path.join(path, 'train_x'), train_x)
@@ -53,7 +60,7 @@ def save_data_to_npy(train_x, train_y, test_x, test_y, path="data"):
 def load_data_from_npy(path="data"):
     """
     load the fer dataset from .npy files
-    :param path: relative path to the current working directory where the npy files are
+    :param path: relative path to the project directory where the npy files are
     :return: train_x: numpy array
     :return: train_y: numpy array
     :return: test_x: numpy array
@@ -65,6 +72,38 @@ def load_data_from_npy(path="data"):
     test_x = np.load(os.path.join(path, 'test_x.npy'))
     test_y = np.load(os.path.join(path, 'test_y.npy'))
     return train_x, train_y, test_x, test_y
+
+
+def remove_npy_files(path="data"):
+    """
+    delete the npy files in the path provided
+    :param path: relative path to the project directory where the npy files are
+    """
+    path = os.path.join(os.path.dirname(__file__), path)
+    try:
+        os.remove(os.path.join(path, 'train_x.npy'))
+        os.remove(os.path.join(path, 'train_y.npy'))
+        os.remove(os.path.join(path, 'test_x.npy'))
+        os.remove(os.path.join(path, 'test_y.npy'))
+    except FileNotFoundError:
+        pass
+
+
+def get_bad_samples(path="data/badtrainingdata.txt"):
+    """
+    Read the bad training data file to filter the dataset.
+    This function reads a file that is located by default at data dir.
+    Every line in the file has an index of a bad sample in the csv file.
+    :param path:
+    :return:
+    """
+    file_path = os.path.join(os.path.dirname(__file__), path)
+    if os.path.isfile(file_path):
+        with open(file_path) as file:
+            return np.array([int(line.strip()) for line in file.readlines()
+                             if not line.isspace() and line.strip().isnumeric()])
+    else:
+        return None
 
 
 def check_if_data_available_in_npy(path="data"):
@@ -106,6 +145,39 @@ def transform_y(data_frame):
     return data_frame
 
 
+def visualize_data(path="data/fer2013.csv"):
+    """
+    Display images from the dataset one by one.
+    :param path: a relative path to the csv file from the project directory
+    """
+    path = os.path.join(os.path.dirname(__file__), path)
+    assert os.path.isfile(path)
+    df = pd.read_csv(path)
+    for pixels in df['pixels']:
+        cv.namedWindow("FER images", cv.WINDOW_NORMAL)
+        cv.imshow("FER images", cv.resize(np.array(pixels.split(), dtype=np.uint8).reshape(48, 48), (768, 768)))
+        if cv.waitKey(0) & 0xFF == ord('q'):
+            break
+
+
+def visualize_bad_data(path="data/fer2013.csv"):
+    """
+    Display bad images in the dataset.
+    :param path: a relative path to the csv file from the project directory
+    """
+    path = os.path.join(os.path.dirname(__file__), path)
+    assert os.path.isfile(path)
+    df = pd.read_csv(path)
+    bad_data_indices = get_bad_samples()
+    bad_data_indices = [] if bad_data_indices is None else bad_data_indices.tolist()
+    for index in bad_data_indices:
+        cv.namedWindow("FER bad images", cv.WINDOW_NORMAL)
+        cv.imshow("FER bad images", cv.resize(np.array(df.iloc[index]['pixels'].split(), dtype=np.uint8).
+                                              reshape(48, 48), (768, 768)))
+        if cv.waitKey(0) & 0xFF == ord('q'):
+            break
+
+
 def print_summary(train_y, test_y):
     """
     print summary about the data
@@ -125,12 +197,19 @@ def print_summary(train_y, test_y):
 
 class DataLoader:
     @staticmethod
-    def load_data(csv_path="data/fer2013.csv"):
+    def load_data(csv_path="data/fer2013.csv", filter_dataset=True):
         if check_if_data_available_in_npy():
             train_x, train_y, test_x, test_y = load_data_from_npy()
         else:
-            train_x, train_y, test_x, test_y = load_data_from_csv(csv_path)
+            train_x, train_y, test_x, test_y = load_data_from_csv(csv_path, filter_dataset=filter_dataset)
             save_data_to_npy(train_x, train_y, test_x, test_y)
+        return train_x, train_y, test_x, test_y
+
+    @staticmethod
+    def reset(csv_path="data/fer2013.csv", filter_dataset=True):
+        remove_npy_files()
+        train_x, train_y, test_x, test_y = load_data_from_csv(csv_path, filter_dataset=filter_dataset)
+        save_data_to_npy(train_x, train_y, test_x, test_y)
         return train_x, train_y, test_x, test_y
 
 
@@ -144,3 +223,9 @@ if __name__ == '__main__':
 
     # print summary
     print_summary(y_train, y_test)
+
+    # visualize the dataset
+    visualize_data()
+
+    # visualize bad data in the dataset
+    visualize_bad_data()
